ee550e85854383bdb50900c308901277a25c9cf5
santiago emanuel mustafa font <santiagomustafa1145@gmail.com>
2025-01-28 12:25:43 -0300

    feat: Implement user registration and logout functionality in auth store

--- store/user.js
+++ store/user.js
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { getItemAsync, setItemAsync } from "expo-secure-store";
+import { deleteItemAsync, getItemAsync, setItemAsync } from "expo-secure-store";
 
 const useAuth = create((set) => ({
   user: null,
@@ -56,6 +56,80 @@ const useAuth = create((set) => ({
     }
 
     return false;
+  },
+
+  /**
+   *
+   * @param {string} dni - DNI del empleado
+   * @param {string} name - Nombre del empleado
+   * @param {'empleado' | 'supervisor'} rol - Rol del empleado
+   * @param {string} password - Contraseña del empleado
+   * @param {string} passwordConfirm - Confirmación de contraseña del empleado
+   * @returns
+   */
+  register: async (dni, name, rol, password, passwordConfirm) => {
+    if (password !== passwordConfirm) {
+      return { success: false, password: "Las contraseñas no coinciden" };
+    }
+
+    if (password.length <= 6) {
+      return { success: false, password: "La contraseña es muy corta" };
+    }
+
+    if (dni.length <= 7) {
+      return { success: false, dni: "Debes ingresar un DNI valido" };
+    }
+
+    if (!name) {
+      return { success: false, name: "Debes ingresar un Nombre" };
+    }
+
+    if (!rol) {
+      return { success: false, rol: "Debes ingresar un rol valido" };
+    }
+
+    const response = await fetch(`https://asa-app-backend.onrender.com/auth/`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        dni,
+        name,
+        rol,
+        password,
+        passwordConfirm,
+      }),
+    })
+      .then((r) => r.json())
+      .then(({ success }) => {
+        if (success) {
+          set(() => ({
+            user: {
+              nombre: name,
+              rol,
+              dni,
+            },
+          }));
+          return { success: true, dni: "Usuario creado" };
+        }
+        return { success: false, dni: "Ya existe un usuario con ese DNI" };
+      })
+      .catch((e) => {
+        console.log({ e }, "error");
+        return {
+          success: false,
+          dni: "Ya existe un usuario con ese DNI",
+        };
+      });
+    return response;
+  },
+  logout: () => {
+    set(() => ({
+      user: null,
+    }));
+
+    deleteItemAsync("sessionToken");
   },
 }));
 
