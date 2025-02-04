442eaec52cf5234e1d9ee9943b6c9aed178afdae
santiago emanuel mustafa font <santiagomustafa1145@gmail.com>
2025-02-04 08:38:39 -0300

    feat: Update TabLayout to conditionally render screens based on user role

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,16 @@
+import useAuth from "@/store/user";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { Tabs } from "expo-router";
 
 export default function TabLayout() {
+  const { user } = useAuth();
+
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: "blue" }}>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "blue",
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
@@ -16,39 +23,43 @@ export default function TabLayout() {
       <Tabs.Screen
         name="ventas"
         options={{
-          title: "Registro de Ventas",
+          title: "Ventas",
           tabBarIcon: ({ color }) => (
             <FontAwesome size={28} name="shopping-cart" color={color} />
           ),
         }}
       />
-      <Tabs.Screen
-        name="distribuidores"
-        options={{
-          title: "Registro de Distribuidores",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome size={28} name="car" color={color} />
-          ),
-        }}
-      />
+      {user?.rol !== "empleado" && (
+        <Tabs.Screen
+          name="distribuidores"
+          options={{
+            title: "Distribuidores",
+            tabBarIcon: ({ color }) => (
+              <FontAwesome size={28} name="car" color={color} />
+            ),
+          }}
+        />
+      )}
       <Tabs.Screen
         name="asistencias"
         options={{
-          title: "Registro de Asistencias",
+          title: "Asistencias",
           tabBarIcon: ({ color }) => (
             <FontAwesome size={28} name="user" color={color} />
           ),
         }}
       />
-      <Tabs.Screen
-        name="resumen"
-        options={{
-          title: "Generar informes",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome size={28} name="area-chart" color={color} />
-          ),
-        }}
-      />
+      {user?.rol !== "empleado" && (
+        <Tabs.Screen
+          name="resumen"
+          options={{
+            title: "Informes",
+            tabBarIcon: ({ color }) => (
+              <FontAwesome size={28} name="area-chart" color={color} />
+            ),
+          }}
+        />
+      )}
     </Tabs>
   );
 }
